03bb4a45aebd988775110958b31ce98b4a5d6ab6
Sergey Valevskyi <sergey.valevskyi@hpe.com>
2018-08-30 16:52:03 +0300

    Add few tests

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,4 +1,10 @@
 from fibonacci import generateFibonacci
 
-def test_fibonacci():
+def test_fibonacci_seven():
     assert generateFibonacci(7) == [0,1,1,2,3,5,8,13]
+
+def test_fibonacci_zero():
+        assert generateFibonacci(0) == [0]
+
+def test_fibonacci_one():
+            assert generateFibonacci(1) == [0,1]
